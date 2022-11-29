5f3ba23376c0d87a420f17e5933d7d31db827703
R <RuqaiahSaif.com>
2022-11-29 10:51:21 +0300

    home

--- app/Http/Controllers/api/adminDashboard/HomepageController.php
+++ app/Http/Controllers/api/adminDashboard/HomepageController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api\adminDashboard;
 
 use App\Models\Homepage;
 use Illuminate\Http\Request;
+use App\Http\Resources\HomepageResource;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\api\BaseController as BaseController;
 
-class HomepageController extends Controller
+class HomepageController extends BaseController
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +17,10 @@ class HomepageController extends Controller
      */
     public function index()
     {
-        //
+        $success['Homepages']=HomepageResource::collection(Homepage::where('is_deleted',0)->get());
+        $success['status']= 200;
+
+         return $this->sendResponse($success,'تم ارجاع الصفحة الرئبسبة  بنجاح','Homepages return successfully');
     }
 
     /**
@@ -35,7 +41,25 @@ class HomepageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->all();
+        $validator =  Validator::make($input ,[
+            'name'=>'required|string|max:255',
+            'key'=>'required'
+        ]);
+        if ($validator->fails())
+        {
+            return $this->sendError(null,$validator->errors());
+        }
+        $Homepage = Homepage::create([
+            'name' => $request->name,
+            'key' => $request->key
+          ]);
+
+
+         $success['Homepages']=New HomepageResource($Homepage );
+        $success['status']= 200;
+
+         return $this->sendResponse($success,'تم إضافةالصفحة بنجاح','Homepage Added successfully');
     }
 
     /**
@@ -44,9 +68,33 @@ class HomepageController extends Controller
      * @param  \App\Models\Homepage  $homepage
      * @return \Illuminate\Http\Response
      */
-    public function show(Homepage $homepage)
+    public function show($homepage)
     {
-        //
+        $Homepage= Homepage::query()->find($homepage);
+        if ($Homepage->is_deleted==1){
+               return $this->sendError("االصفحة غير موجودة","Homepage is't exists");
+               }
+              $success['homepages']=New HomepageResource($Homepage);
+              $success['status']= 200;
+
+               return $this->sendResponse($success,'تم عرض الصفحة بنجاح','Homepage showed successfully');
+    }
+    public function changeStatus($id)
+    {
+        $Homepage = Homepage::query()->find($id);
+        if ($Homepage->is_deleted==1){
+         return $this->sendError("الصفحة غير موجودة","Homepage is't exists");
+         }
+        if($Homepage->status === 'active'){
+            $Homepage->update(['status' => 'not_active']);
+     }
+    else{
+        $Homepage->update(['status' => 'active']);
+    }
+        $success['homepages']=New HomepageResource($Homepage);
+        $success['status']= 200;
+         return $this->sendResponse($success,'تم تعدبل حالة الصفحة بنجاح',' Homepage status updared successfully');
+
     }
 
     /**
@@ -69,7 +117,28 @@ class HomepageController extends Controller
      */
     public function update(Request $request, Homepage $homepage)
     {
-        //
+        if ($homepage->is_deleted==1){
+            return $this->sendError("الصفحة غير موجودة"," homepage is't exists");
+       }
+            $input = $request->all();
+           $validator =  Validator::make($input ,[
+                'name'=>'required|string|max:255',
+                'key'=>'required',
+           ]);
+           if ($validator->fails())
+           {
+               # code...
+               return $this->sendError(null,$validator->errors());
+           }
+           $homepage->update([
+               'name' => $request->input('name'),
+               'key' => $request->input('key'),
+           ]);
+
+           $success['homepages']=New homepageResource($homepage);
+           $success['status']= 200;
+
+            return $this->sendResponse($success,'تم التعديل بنجاح','homepage updated successfully');
     }
 
     /**
@@ -78,8 +147,17 @@ class HomepageController extends Controller
      * @param  \App\Models\Homepage  $homepage
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Homepage $homepage)
+    public function destroy($homepage)
     {
-        //
+        $homepage =Homepage::query()->find($homepage);
+        if ($homepage->is_deleted==1){
+            return $this->sendError("الصفحة غير موجودة","Homepage is't exists");
+            }
+           $homepage->update(['is_deleted' => 1]);
+
+           $success['homepages']=New HomepageResource($homepage);
+           $success['status']= 200;
+
+            return $this->sendResponse($success,'تم حذف الصفحة بنجاح','Homepage deleted successfully');
     }
 }
